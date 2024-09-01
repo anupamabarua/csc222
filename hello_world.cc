df290da344e6f3120bf98c7857452f0366cbd59e
anupamab <anupamabaruaa@gmail.com>
2024-09-01 01:45:51 -0400

    random

---
+++ hello_world.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+/Users/1003113/projects/csc222 
+using namespace std;
+
+// main: generate some simple output
+
+int main()
+{
+    int num = 42;
+
+    cout << "The answer to the ultimate question of life, ";
+    cout << "the universe and everything is (of course) ";
+    cout << num  << "." << endl;
+
+    return 0;
+}
