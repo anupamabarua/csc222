e8adb57ded5cd82a440c5f6eb1d2c592911d4b97
anupamab <anupamabaruaa@gmail.com>
2024-09-05 01:27:56 -0400

    ten_mathematical_expressions.cpp

---
+++ chapterexercises/chapter2/ten_mathematical_expressions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int n = 5;
+int m = 7;
+
+cout << "The product of " << n << " and " << m << " is ";
+cout << n * m << "." << endl;
+
+cout << "The sum of " << n << " and " << m << " is ";
+cout << n + m << "." << endl;
+    
+cout << "The difference between " << n << " and " << m << " is ";
+cout << n - m << "." << endl;
+    
+cout << "The product of " << n << " and " << m << " is ";
+cout << n * m << "." << endl;
+    
+cout << "The quotient of " << n << " divided by " << m << " is ";
+cout << n / m << "." << endl;
+    
+cout << "The remainder of " << n << " divided by " << m << " is ";
+cout << n % m << "." << endl;
+  
+return 0;
+}
