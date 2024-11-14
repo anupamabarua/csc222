061a9045e516257b99a44494af2b7d423c06a101
anupamab <anupamabaruaa@gmail.com>
2024-11-14 12:06:19 -0500

    part 2 works

--- chap10exercise/num_vector_utils.cpp
+++ chap10exercise/num_vector_utils.cpp
@@ -2,7 +2,14 @@
 #include <vector>
 using namespace std;
 
-string render_num_vector(const vector<int>&) {
+string render_num_vector(const vector<int>& nums) {
     string s = "";
+    for(int i=0;i<nums.size();i++){
+        s.append(std::to_string(nums[i]));
+        if(i != nums.size()-1){
+            s.append(" ");
+        }
+    }
+
     return s;
 }
