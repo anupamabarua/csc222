972e6a8530420839d47c3504925c021975e52820
anupamab <anupamabaruaa@gmail.com>
2024-11-14 13:10:33 -0500

    passed

--- chap10exercise/num_vector_utils.cpp
+++ chap10exercise/num_vector_utils.cpp
@@ -6,7 +6,11 @@ string render_num_vector(const vector<int>& nums) {
     string s = "1 3 7";
     return s;
 }
+int num_vector_sum(const vector<int>& v) {
+	int nums = 0;
+	for(int i = 0; i < sizeof(v); i++){
+		nums += v[i];
+} 	
 
-int num_vector_sum(const vector<int>&) {
-              return 0;
+return nums;
 }
