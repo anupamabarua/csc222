300463c8644b5c3b1049fad52341a3cd6246251a
anupamab <anupamabaruaa@gmail.com>
2024-11-19 21:14:23 -0500

    did example 6 im done for the day

--- chap11/time.cpp
+++ chap11/time.cpp
@@ -8,6 +8,7 @@ struct Time {
     void increment(double secs);
 
     double convert_to_seconds() const;
+    bool after(const Time& t2) const;
 };
 
 void Time::print()
@@ -33,11 +34,26 @@ double Time::convert_to_seconds() const
 {
     return (hour * 60 + minute) * 60 + second;
 }
+
+bool Time::after(const Time& t2) const
+{
+    if (hour > t2.hour) return true;
+    if (hour < t2.hour) return false;
+
+    if (minute > t2.minute) return true;
+    if (minute < t2.minute) return false;
+
+    return (second > t2.second);
+}
 int main() {
     Time current_time = {9, 14, 30.0};
     current_time.increment(500.0);
     current_time.print();
     cout << current_time.convert_to_seconds() << endl;
-    return 0;
+    Time done_time = {10, 14, 31.0};
+    if (done_time.after(current_time)) {
+    cout << "The bread will be done after it starts." << endl;
+} 
+   return 0;
 }
 
