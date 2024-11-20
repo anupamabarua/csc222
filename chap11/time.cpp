f2da4ac79c503b0af24ecdac823d418ca7b8f906
anupamab <anupamabaruaa@gmail.com>
2024-11-19 20:41:34 -0500

    finished example 3 this is lowkey making sense for once

--- chap11/time.cpp
+++ chap11/time.cpp
@@ -5,6 +5,7 @@ struct Time {
     int hour, minute;
     double second;
     void print();
+    void increment(double secs);
 };
 
 void Time::print()
@@ -12,8 +13,22 @@ void Time::print()
     cout << hour << ":" << minute << ":" << second << endl;
 }
 
+void Time::increment(double secs)
+{
+    second += secs;
+
+    while (second >= 60.0) {
+        second -= 60.0;
+        minute += 1;
+    }
+    while (minute >= 60) {
+        minute -= 60;
+        hour += 1;
+    }
+}
 int main() {
     Time current_time = {9, 14, 30.0};
+    current_time.increment(500.0);
     current_time.print();
     return 0;
 }
