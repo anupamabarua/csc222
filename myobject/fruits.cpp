ff48100f98e264f81b4a49d1bb5ea11edfed21af
anupamab <anupamabaruaa@gmail.com>
2024-12-06 22:07:59 -0500

    everything should work now

--- myobject/fruits.cpp
+++ myobject/fruits.cpp
@@ -6,17 +6,20 @@ using namespace std;
 
 vector<string> make_list(stringstream& ss, char listnum) {
     vector<string> fruits;
-    string fruits;
+    string fruit;
     if (listnum == 1) {
-        while ((ss >> fruits) && (fruits != "==="))
-            fruits.push_back(fruits);
+        while ((ss >> fruit) && (fruit != "==="))
+            fruits.push_back(fruit);
     } else if (listnum == 2) {
-        while ((ss >> fruits) && (fruits != "==="));
-        while (ss >> fruits) fruits.push_back(fruits);
+        while ((ss >> fruit) && (fruit != "===")) {}  // Skip first list
+        while (ss >> fruit) 
+            fruits.push_back(fruit);
     }
     return fruits;
 }
+
 vector<string> make_pairs(vector<string> fruits) {
     vector<string> pairs;
     return pairs;
 }
+
--- myobject/test_fruits.cpp
+++ myobject/test_fruits.cpp
@@ -9,6 +9,16 @@ using namespace std;
 string input = "CPE1\nCPE2\nCPE3\nCPE4\n===\nNC1\nNC2\nNC3\n";
 stringstream ss(input);
 
-TEST_CASE("Test can create and render fruit") {
-    MyObject myobj();
-    CHECK(myobj.to_string() == "This is fruit");
+TEST_CASE("Test make_list function") {
+    vector<string> list1 = make_list(ss, 1);
+    CHECK(list1 == vector<string>{"CPE1", "CPE2", "CPE3", "CPE4"});
+
+    stringstream ss2(input);
+    vector<string> list2 = make_list(ss2, 2);
+    CHECK(list2 == vector<string>{"NC1", "NC2", "NC3"});
+}
+
+TEST_CASE("Test make_pairs function") {
+    vector<string> fruits = {"CPE1", "CPE2", "CPE3", "CPE4"};
+    vector<string> pairs = make_pairs(fruits);
+}
