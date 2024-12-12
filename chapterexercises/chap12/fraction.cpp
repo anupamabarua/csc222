eca434543cd6a90bd69b0624a4b140a5e1dc4a67
Barua <anupamabaruaagmail.com>
2024-12-12 15:59:27 -0500

    it just keeps on telling me doctest.h dosent exist

--- chapterexercises/chap12/fraction.cpp
+++ chapterexercises/chap12/fraction.cpp
@@ -2,3 +2,12 @@
 #include "Fraction.h"
 #include <iostream>
 using namespace std;
+
+Fraction::Fraction(int n, int d) {
+	numerator = n;
+	denominator = d;
+}
+Fraction::Fraction() {
+	numerator = 0;
+	denominator = 1;
+}
--- chapterexercises/chap12/fraction.h
+++ chapterexercises/chap12/fraction.h
@@ -2,3 +2,13 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+struct Fraction {
+int numerator, denominator;
+    Fraction(int n, int d);
+    Fraction();
+    void print();
+};
+
+
+
