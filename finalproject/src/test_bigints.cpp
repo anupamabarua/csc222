c0920f61c534c34631582ca703c879540e74e21f
Barua <anupamabaruaagmail.com>
2025-01-21 12:14:14 -0500

    made test for < inequality

--- finalproject/src/test_bigints.cpp
+++ finalproject/src/test_bigints.cpp
@@ -37,9 +37,22 @@ TEST_CASE("Test can compare BigInts for inequality") {
     BigInt i4("-654321");
     BigInt i5("54321");
     BigInt i6("-54321");
+    
     CHECK((i2 > i5) == false);
     CHECK((i2 > i1) == true);
     CHECK((i4 > i1) == false);
     CHECK((i6 > i4) == true);
 }
 
+TEST_CASE("test can compare bigints for <") {
+    BigInt i1("12345");
+    BigInt i2("54321");
+    BigInt i3("123456");
+    BigInt i4("-654321");
+    BigInt i5("54321");
+    BigInt i6("-54321");
+
+    CHECK((i1 < i1) == false);
+    CHECK((i1 < i3) == true);
+    CHECK((i2 < i1) == true);
+    CHECK((i2 < i5) == false);
