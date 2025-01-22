713cc61e684103eabd91e09f5925e262053add00
Barua <anupamabaruaagmail.com>
2025-01-22 12:49:04 -0500

    test <= fail

--- finalproject/src/BigInt.cpp
+++ finalproject/src/BigInt.cpp
@@ -86,3 +86,7 @@ bool BigInt::operator<(const BigInt& num1) const{
 bool BigInt::operator!=(const BigInt& num1) const{
     return !(*this == num1);
 }
+bool BigInt::operator<=(const BigInt& num1) const{
+    return !(*this == num1);
+}
+
